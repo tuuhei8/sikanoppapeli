b559da6e03675a68d9752eef2c8ecb62956e9473
Tuukka Heiskanen <tuukkaheiskanen5@gmail.com>
2025-01-12 23:15:59 +0200

    päivitys

--- main.js
+++ main.js
@@ -1,20 +1,36 @@
+// Kuuntele pelaajien määrän vahvistusta:
 let pelaajienMaara = document.getElementById('pelaajienMaara');
 pelaajienMaara.addEventListener('submit', pelaajaAsetukset);
+
+// Kuuntele pelisääntöjen vahvistusta:
 let saannot = document.getElementById('peliSaannot');
 saannot.addEventListener('submit', peliSaannot);
-let pelaajaLista = [];
-let pisteTavoite = 0
-let noppienMaara = 0
 
+// Kuuntele "lopeta vuoro" nappia:
+let kenenVuoro = document.getElementById('lopetaVuoro');
+kenenVuoro.addEventListener('click', seuraavaVuoro);
+
+// Kuuntele "heitä" nappia:
+let heita = document.getElementById('heita');
+heita.addEventListener('click', heitto)
+
+// Muuttujia, pelaajien nimet, pisteet ja muut asetukset:
+let pelaajaLista = [];
+let pisteTavoite = 0;
+let noppienMaara = 0;
+let vuoro = 0;
+let tamanVuoronPisteet = 0;
+
+//Pelaajien määrän ja nimien asetus:
 function pelaajaAsetukset(event) {
     event.preventDefault();
     let pelaajia = document.getElementById('pelaajia').value;
-    // Tarkistetaan että pelaajien määräksi on annettu numero:
+    // Tarkistaa että pelaajien määräksi on annettu numero:
     if (isNaN(pelaajia) || pelaajia.length < 1) {
         document.getElementById('pitaaOllaNumero').innerHTML = 'Anna numero.';
         return;
     } else {
-        // Kysytään nimet ja kutstutaan funktiota jolla luodaan array:
+        // Kysyy nimet:
         document.getElementById('pitaaOllaNumero').innerHTML = '';
         let text = '<form>';
         for (let i = 0; i < pelaajia; i++) {
@@ -22,27 +38,29 @@ function pelaajaAsetukset(event) {
             text += `<p>Pelaajan ${j} nimi:</p>` + '<input type="text" name="nimi">';
         }
         text += '<p id="nimetVaroitus"></p><div><button id="vahvistaNimet">Vahvista</button></div></form>';
+        // Kuuntelee asetettujen nimien vahvistusta ja kutsu funktiota arrayn luomiseksi:
         document.getElementById('nimet').innerHTML = text;
         let vahvistaNimet = document.getElementById('vahvistaNimet');
         vahvistaNimet.addEventListener('click', pelaajatArray);
     }
 }
 
-// luodaan array joka sisältää objektit joissa nimet ja pisteet:
+// luo arrayn joka sisältää objektit joissa nimet ja pisteet:
 function pelaajatArray(event) {
     event.preventDefault();
     let ok = true;
     let tarkistusLista = [];
     let nimiLista = document.querySelectorAll('input[name="nimi"]');
-    // Tarkistetaan että nimet on täytetty oikein:
+    // Tarkistaa että nimet on täytetty oikein:
     for (let i = 0; i < nimiLista.length; i++) {
-        if (nimiLista[i].value.length < 1 || nimiLista[i].value.length > 20 || tarkistusLista.includes(nimiLista[i].value)) {
+        let nimi = nimiLista[i].value
+        if (nimi.length < 1 || nimi.length > 20 || tarkistusLista.includes(nimi)) {
             ok = false;
         }
         tarkistusLista.push(nimiLista[i].value);
     }
+    // Muodostaa arrayn ja avaa seuraavan valikon:
     if (ok == true) {
-        // Muodostetaan array ja avataan seuraava valikko:
         document.getElementById('nimet').style.display = 'none';
         document.getElementById('pelaajienMaara').style.display = 'none';
         for (let i = 0; i < nimiLista.length; i++) {
@@ -58,9 +76,9 @@ function pelaajatArray(event) {
     console.log(pelaajaLista)
 }
 
+// Tarkistaa voittoon tarvittavan pistemäärän ja käytettävien noppien määrän asetukset:
 function peliSaannot(event) {
     event.preventDefault();
-    // Tarkistetaan voittoon tarvittava pistemäärä ja käytettävien noppien määrä:
     let ok1 = false;
     let ok2 = false;
     let pisteet = document.forms['saannot']['pisteMaara'].value;
@@ -82,7 +100,66 @@ function peliSaannot(event) {
     if (ok1 == true && ok2 == true) {
         console.log('ok', ok1, ok2, typeof noppienMaara, typeof pisteTavoite)
         document.getElementById('saantoValikko').style.display = 'none';
+        uusiVuoro()
     }
+}
+
+// Asettaa käyttöliittymän uuden vuoron alkuun:
+function uusiVuoro() {
+    tamanVuoronPisteet = 0
+    pisteTaulukko()
+    let noppaKuvat = document.getElementById('noppaKuvat')
+    if (noppienMaara == 1) {
+        noppaKuvat.innerHTML = '<img class="col-sm-3" src="img/tyhja.png" alt="noppa">'
+    } else {
+        noppaKuvat.innerHTML = '<img class="col-sm-3" src="img/tyhja.png" alt="noppa"><img class="col-md-3" src="img/tyhja.png" alt="noppa">'
+    }
+    document.getElementById('pelaajaNimi').innerHTML = pelaajaLista[vuoro].nimi
+    document.getElementById('tulos').innerHTML = 0
+    document.getElementById('yhteensa').innerHTML = tamanVuoronPisteet
+    document.getElementById('kaikkiYhteensa').innerHTML = pelaajaLista[vuoro].pisteet
+}
+
+// Lisää pisteet ja käynnistää seuraavan pelaajan vuoron:
+function seuraavaVuoro() {
+    pelaajaLista[vuoro].pisteet =  pelaajaLista[vuoro].pisteet + tamanVuoronPisteet
+    if (vuoro == pelaajaLista.length - 1) {
+        vuoro = 0
+    } else {
+        vuoro++
+    }
+    uusiVuoro()
+    console.log(vuoro,'s', pelaajaLista[0].pisteet)
+}
+
+// Muodostaa pistetaulukon HTML:ään:
+function pisteTaulukko() {
+    let text = ''
+    for (let i = 0; i < pelaajaLista.length; i++) {
+        text += `<tr><td>${pelaajaLista[i].nimi}</td><td>${pelaajaLista[i].pisteet}</td></tr>`
+    }
+    document.getElementById('pisteTaulu').innerHTML = text
+}
+
+// Suorittaa nopan tai noppien heiton ja laskee tuloksen ja pisteet:
+function heitto() {
+    const luku1 = Math.floor(Math.random() * 6) + 1
+    const luku2 = Math.floor(Math.random() * 6) + 1
+    const noppa1 = 'img/luku' + `${luku1}` + '.png'
+    const noppa2 = 'img/luku' + `${luku2}` + '.png'
+    if (noppienMaara == 1) {
+        document.querySelectorAll('img')[0].setAttribute('src', noppa1)
+        document.getElementById('tulos').innerHTML = luku1
+        tamanVuoronPisteet += luku1
+    } else {
+        document.querySelectorAll('img')[0].setAttribute('src', noppa1)
+        document.querySelectorAll('img')[1].setAttribute('src', noppa2)
+        let tulo = luku1 + luku2
+        document.getElementById('tulos').innerHTML = tulo
+        tamanVuoronPisteet += tulo
+    }
+    document.getElementById('yhteensa').innerHTML = tamanVuoronPisteet
+    document.getElementById('kaikkiYhteensa').innerHTML = tamanVuoronPisteet + pelaajaLista[vuoro].pisteet
 }
 
 /*
